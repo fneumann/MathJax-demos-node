91b6ca9af749b7ae5791deab238fd350178722ff
fneumann <fneumann@databay.de>
2024-05-28 13:52:55 +0200

    Add more trial outputs

--- run-tryout.sh
+++ run-tryout.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+node -r esm speech/tex2chtml "e=m*c^2" >tryout/speech-tex2chtml.html
+node -r esm speech/tex2chtml-speech "e=m*c^2" >tryout/speech-tex2chtml-speech.html
+node -r esm speech/tex2speech "e=m*c^2" >tryout/speech-tex2speech.html
+node -r esm speech/tex2svg "e=m*c^2" >tryout/speech-tex2svg.html
+exit;
+
 # direct
 node -r esm direct/tex2chtml-page tryout/example.html >tryout/direct-tex2chtml-page.html
 node -r esm direct/tex2svg-page tryout/example.html >tryout/direct-tex2svg-page.html
@@ -15,6 +21,8 @@ node -r esm preload/tex2svg-page tryout/example.html >tryout/preload-tex2svg-pag
 # simple
 node -r esm simple/tex2chtml-page tryout/example.html >tryout/simple-tex2chtml-page.html
 node -r esm simple/tex2svg-page tryout/example.html >tryout/simple-tex2svg-page.html
+node -r esm simple/tex2chtml "e=m*c^2" >tryout/simple-tex2chtml.html
+node -r esm simple/tex2svg "e=m*c^2" >tryout/simple-tex2svg.svg
 
 # speech
 node -r esm speech/tex2chtml-speech-page tryout/example.html >tryout/speech-tex2chtml-speech-page.html
